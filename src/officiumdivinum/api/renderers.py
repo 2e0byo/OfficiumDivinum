6dbb182dd9dc6bc354024b093fe4805eb03e50c0
John Maximilian <2e0byo@gmail.com>
2021-02-10 19:01:19 +0000

    handle translations

--- src/officiumdivinum/api/renderers.py
+++ src/officiumdivinum/api/renderers.py
@@ -18,22 +18,21 @@ class objectHTMLRenderer(BaseRenderer):
         except KeyError:
             page = None
 
-        content = []
-        for thing in data:
-            content.append(thing.html())
-
         if page:
+            if len(data) == 2:
+                content, translation = data
+                content = " ".join([x.html() for x in content])
+                translation = " ".join([x.html() for x in translation])
+            else:
+                content = " ".join([x.html() for x in data])
+                translation = None
             page = {}
             page["title"] = data[0].name
             page["liturgical_day"] = data[0].liturgical_day
             page["calendar_day"] = data[0].calendar_day
-            if len(content) == 2:
-                content, translation = content
-            else:
-                content = content[0]
-                translation = None
             return render_template(
                 "html/page.html", content=content, translation=translation, page=page
             )
         else:
-            return " ".join(content)
+            content = " ".join([x.html() for x in data])
+            return content
